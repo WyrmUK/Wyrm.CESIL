1fb20ea15ae5ad8de67b809131a0a4f54fc91a68
Laurence Mee <lmee@wyrm.org.uk>
2024-08-08 12:44:15 +0200

    Added Token tests.

---
+++ tests/Wyrm.CESIL.UnitTests/Lexical/TokenTests.cs
@@ -0,0 +1,21 @@
+﻿using Shouldly;
+using Wyrm.CESIL.Lexical;
+
+namespace Wyrm.CESIL.UnitTests.Lexical;
+
+public class TokenTests
+{
+    [Fact]
+    public void Token_Constructor_Should_Set_Values()
+    {
+        const int lineNo = 101;
+        const int charNo = 18;
+        const TokenType tokenType = TokenType.Comment;
+        const string value = "## Comment";
+        var token = new Token(lineNo, charNo, tokenType, value);
+        token.LineNo.ShouldBe(lineNo);
+        token.CharNo.ShouldBe(charNo);
+        token.TokenType.ShouldBe(tokenType);
+        token.Value.ShouldBe(value);
+    }
+}
